4402b35e5e1fdcf8b7052a2ae0bdb8e26daa99b8
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-08-15 09:55:17 +0200

    Finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,11 +8,16 @@ import Cart from './Cart';
 import ShoppingList from './ShoppingList';
 import Recommendation from './Recommendation';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [cart, updateCart] = useState([])
+  const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : []);
+	
+  useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
 
   return (
     <div className="App">
